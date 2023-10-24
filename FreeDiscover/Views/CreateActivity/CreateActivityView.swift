0f677a0ebb4751985cfa2828c802864c6aff54a2
victorlmtr <victor.lemaitre1@gmail.com>
2023-10-24 14:55:52 +0200

    profileimage+createactivity+categorypicker

--- FreeDiscover/Views/CreateActivity/CreateActivityView.swift
+++ FreeDiscover/Views/CreateActivity/CreateActivityView.swift
@@ -14,9 +14,9 @@ enum DurationMode {
 
 
 struct CreateActivityView: View {
-    @State private var currentMode: DurationMode = .temporary
+    @State var currentMode: DurationMode = .temporary
     //  @State private var currentCategory: CategoryMode = .nature
-    @State private var currentActivityType = "Nature"
+  //  @State private var currentActivityType = "Nature"
     // let activityTypes =  ["Nature", "Culture", "Sport", "Social"]
     @State private var dates: Set<DateComponents> = []
     @State private var showingSheet = false
@@ -30,6 +30,24 @@ struct CreateActivityView: View {
     
     var body : some View {
         VStack {
+            CategoryPicker(activityCategory: .nature)
+                .padding()
+            
+            TextField(("Nom de l'activité"), 
+                      text: $activityName)
+                .padding()
+                .background(Color.grayLight)
+                .cornerRadius(5.0)
+                .padding()
+            TextField(("Description de l'activité"),
+                      text: $activityDescription,
+                      axis: .vertical)
+                .lineLimit(3...4)
+                .background(Color.grayLight)
+                .cornerRadius(5.0)
+                .padding()
+            //    .textFieldStyle(.roundedBorder)
+            
             ZStack{
                 RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
                     .frame(width: 340,height: 40)
@@ -58,94 +76,6 @@ struct CreateActivityView: View {
                     
                 }
             }
-            ZStack{
-                RoundedRectangle(cornerRadius: 5)
-                    .frame(width: 346,height: 44)
-                    .shadow(radius: 4)
-                    .foregroundColor(.white)
-                    // RoundedRectangle(cornerRadius: 25)
-                //    .frame(width: 60, height: 36)
-                //    .offset(x: activityCategory == .nature ? -102 : 102)
-                 //   .foregroundColor(Color.accentColor)
-                HStack{
-                    
-                    Button(action: {activityCategory = .nature}, label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 80, height: 40)
-                                .foregroundColor(activityCategory == .nature ? .accentColor : Color.clear)
-                            VStack {
-                                Image(systemName: "leaf")
-                                    .foregroundColor(activityCategory == .nature ? .white : Color.grayDark)
-                                Text("Nature")
-                                    .font(.footnote)
-                                    .bold()
-                                    .foregroundColor(activityCategory == .nature ? .white : Color.grayDark)
-                                    .frame(width: 80)
-                                
-                            }
-                        }
-            
-                    })
-                    Button(action: {activityCategory = .culture}, label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 80, height: 40)
-                                .foregroundColor(activityCategory == .culture ? .accentColor : Color.clear)
-                            VStack {
-                                Image(systemName: "building.columns")
-                                    .foregroundColor(activityCategory == .culture ? .white : Color.grayDark)
-                                Text("Culture")
-                                    .font(.footnote)
-                                    .bold()
-                                    .foregroundColor(activityCategory == .culture ? .white : Color.grayDark)
-                                    .frame(width: 80)
-                                
-                            }
-                        }
-            
-                    })
-                    Button(action: {activityCategory = .sport}, label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 80, height: 40)
-                                .foregroundColor(activityCategory == .sport ? .accentColor : Color.clear)
-                            VStack {
-                                Image(systemName: "figure.walk")
-                                    .foregroundColor(activityCategory == .sport ? .white : Color.grayDark)
-                                Text("Sport")
-                                    .font(.footnote)
-                                    .bold()
-                                    .foregroundColor(activityCategory == .sport ? .white : Color.grayDark)
-                                    .frame(width: 80)
-                                
-                            }
-                        }
-            
-                    })
-                    Button(action: {activityCategory = .social}, label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 80, height: 40)
-                                .foregroundColor(activityCategory == .social ? .accentColor : Color.clear)
-                            VStack {
-                                Image(systemName: "figure.socialdance")
-                                    .foregroundColor(activityCategory == .social ? .white : Color.grayDark)
-                                Text("Social")
-                                    .font(.footnote)
-                                    .bold()
-                                    .foregroundColor(activityCategory == .social ? .white : Color.grayDark)
-                                    .frame(width: 80)
-                                
-                            }
-                        }
-            
-                    })
-                    
-                 
-                    
-                }
-            }
             
            
             if currentMode == .temporary {
@@ -164,19 +94,7 @@ struct CreateActivityView: View {
         }
         
    
-        TextField(("Nom de l'activité"), text: $activityName)
-            .padding()
-            .background(Color.grayLight)
-            .cornerRadius(5.0)
-            .padding(.bottom, 20)
-        TextField(("Description de l'activité"), text: $activityDescription, axis: .vertical)
-            .lineLimit(3)
-            .padding()
-            .background(Color.grayLight)
-            .cornerRadius(5.0)
-            .padding(.bottom, 20)
-        //    .textFieldStyle(.roundedBorder)
-        
+      
         
 //       Button(action: {
 //           print("pouet")
@@ -199,6 +117,12 @@ struct CreateActivityView: View {
         .sheet(isPresented: $showingSheet) {
             MultiDatePicker("Sélectionner une date", selection: $dates)
         }
+        Button(action: {
+            print("pouet")
+        }) {
+            CtaButton(ctaText: "Ajouter une photo/vidéo", ctaIcon: "photo.badge.plus", ctaBgColor: .lightBlue, ctaFgColor: .grayDark)
+                .frame(minWidth: 340)
+        }
         HStack {
             Button(action: {
                 self.symbolSelection = 1
@@ -213,7 +137,7 @@ struct CreateActivityView: View {
             
         }
         Spacer()
-        CtaButton(ctaText: "Créer mon activité", ctaIcon: "calendar", ctaBgColor: .grayLight, ctaFgColor: .accentColor)
+        CtaButton(ctaText: "Créer mon activité", ctaIcon: "", ctaBgColor: .grayLight, ctaFgColor: .accentColor)
             .padding()
     }
 }
