48e124e6f5f55a4fb1a579384135aa35823cf35b
victorlmtr <victor.lemaitre1@gmail.com>
2023-10-24 11:28:53 +0200

    createactivity : upload user image

---
+++ FreeDiscover/Views/CreateActivity/ProfileImage.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileImage.swift
+//  FreeDiscover
+//
+//  Created by apprenant92 on 24/10/2023.
+//
+
+import Foundation
---
+++ FreeDiscover/Views/CreateActivity/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  FreeDiscover
+//
+//  Created by apprenant92 on 24/10/2023.
+//
+
+import Foundation
