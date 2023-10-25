c11ef1b5f5afff6e69d73cb9f7e71a5028a64b35
victorlmtr <victor.lemaitre1@gmail.com>
2023-10-25 09:19:04 +0200

    createactivity update (toggle) + likebutton

--- FreeDiscover/Componants/Buttons/LikeButton.swift
+++ FreeDiscover/Componants/Buttons/LikeButton.swift
@@ -5,4 +5,22 @@
 //  Created by apprenant92 on 24/10/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct LikeButton : View {
+    @State var isPressed = false
+    var body: some View {
+        ZStack {
+            Image(systemName: "heart.fill")
+                .opacity(isPressed ? 1 : 0)
+                .scaleEffect(isPressed ? 1.0 : 0.1)
+                .animation(.linear)
+            
+            Image(systemName: "heart")
+        }.font(.system(size: 40))
+            .onTapGesture {
+                self.isPressed.toggle()
+        }
+        .foregroundColor(isPressed ? .red : .white)
+    }
+}
