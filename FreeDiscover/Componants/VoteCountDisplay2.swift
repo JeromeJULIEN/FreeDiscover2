62ea2df77d008060f8914a36b68537f9275a3a9b
victorlmtr <victor.lemaitre1@gmail.com>
2023-10-31 09:35:54 +0100

    LocationPicker ok, Votecountdisplay2

---
+++ FreeDiscover/Componants/VoteCountDisplay2.swift
@@ -0,0 +1,8 @@
+//
+//  VoteCountDisplay2.swift
+//  FreeDiscover
+//
+//  Created by apprenant92 on 30/10/2023.
+//
+
+import Foundation
---
+++ FreeDiscover/Views/CreateActivity/LocationPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPickerView.swift
+//  FreeDiscover
+//
+//  Created by apprenant92 on 30/10/2023.
+//
+
+import SwiftUI
+
+struct LocationPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPickerView()
+}
