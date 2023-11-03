c8bcafd93f1635508c3f3cfb7eee8405608efd52
victorlmtr <victor.lemaitre1@gmail.com>
2023-11-03 09:06:57 +0100

    Homepage overlay, mapview fix...

---
+++ FreeDiscover/Componants/ListCardLarge.swift
@@ -0,0 +1,18 @@
+//
+//  ListCardLarge.swift
+//  FreeDiscover
+//
+//  Created by apprenant92 on 02/11/2023.
+//
+
+import SwiftUI
+
+struct ListCardLarge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListCardLarge()
+}
---
+++ FreeDiscover/Views/Homepage/ActivityPreviewManager.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityPreviewManager.swift
+//  FreeDiscover
+//
+//  Created by apprenant92 on 02/11/2023.
+//
+
+import Foundation
